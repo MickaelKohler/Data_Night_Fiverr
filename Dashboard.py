f51526ff8fa3a3b4b2a99c616a0338c97c90faff
Maxime <maxime.nicastro@gmail.com>
2021-06-29 14:39:33 +0200

    .

--- Dashboard.py
+++ Dashboard.py
@@ -1,18 +1,17 @@
 import pandas as ps
 import streamlit as st
 
-st.markdown("""  <style> .reportview-container { background:
-    url("https://github.com/MickaelKohler/Data_Night_Fiverr/blob/main/image_fiverr2.png?raw=true")}
-    </style> """, unsafe_allow_html=True)
 
 st.sidebar.title('Hackathon Challenge')
 st.sidebar.subheader('Navigation')
 
-categorie = st.sidebar.radio("Categories", ("Home", "Data Cleaning","DataViz",
-                                            "Maching Learning","NLP"))
+categorie = st.sidebar.radio("Categories", ("Home", "Clustering", "NLP"))
 
 
 if categorie == 'Home':
+    st.markdown("""  <style> .reportview-container { background:
+    url("https://github.com/MickaelKohler/Data_Night_Fiverr/blob/main/image_fiverr2.png?raw=true")}
+    </style> """, unsafe_allow_html=True)
     st.markdown("***")
     st.title('Home')
     st.write("Context:")
@@ -20,20 +19,12 @@ if categorie == 'Home':
     st.write("Problematic:")
     st.write("Your objective is to advise the Fiverr freelancers on how to fill efficiently their Seller Profile. To find, quantify and interpret the quality criteria. Use all the techniques you know, in particular NLP for text processing, and supervised or unsupervised machine learning.")
 
-if categorie == 'Data Cleaning':
+if categorie == 'Clustering':
     st.markdown("***")
-    st.title('Data Cleaning')
-
-if categorie == 'DataViz':
-    st.markdown("***")
-    st.title('DataViz')
-
-if categorie == 'Maching Learning':
-    st.markdown("***")
-    st.title('Maching Learning')
+    st.title('Clustering')
 
 if categorie == 'NLP':
     st.markdown("***")
     st.title('NLP')
-    st.image("https://github.com/MickaelKohler/Data_Night_Fiverr/blob/main/title_schema.png?raw=true")
+    st.image("https://github.com/MickaelKohler/Data_Night_Fiverr/blob/main/title_shema1.png?raw=true")
 
