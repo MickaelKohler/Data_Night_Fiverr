c448ccbfb0b572d7fa34477314b719b5e40b6c71
Maxime <maxime.nicastro@gmail.com>
2021-06-29 14:01:58 +0200

    .

--- Dashboard.py
+++ Dashboard.py
@@ -8,13 +8,17 @@ st.markdown("""  <style> .reportview-container { background:
 st.sidebar.title('Hackathon Challenge')
 st.sidebar.subheader('Navigation')
 
-categorie = st.sidebar.radio("Categories", ("Accueil", "Data Cleaning","DataViz",
+categorie = st.sidebar.radio("Categories", ("Home", "Data Cleaning","DataViz",
                                             "Maching Learning","NLP"))
 
 
-if categorie == 'Accueil':
+if categorie == 'Home':
     st.markdown("***")
-    st.title('Accueil')
+    st.title('Home')
+    st.write("Context:")
+    st.write("Tons of gigs are made every day, and so Fiverr's algorithm highlights certain seller profiles more than others. This is where rules could be established to support the freelancers community.")
+    st.write("Problematic:")
+    st.write("Your objective is to advise the Fiverr freelancers on how to fill efficiently their Seller Profile. To find, quantify and interpret the quality criteria. Use all the techniques you know, in particular NLP for text processing, and supervised or unsupervised machine learning.")
 
 if categorie == 'Data Cleaning':
     st.markdown("***")
@@ -31,4 +35,5 @@ if categorie == 'Maching Learning':
 if categorie == 'NLP':
     st.markdown("***")
     st.title('NLP')
+    st.image("https://github.com/MickaelKohler/Data_Night_Fiverr/blob/main/title_schema.png?raw=true")
 
