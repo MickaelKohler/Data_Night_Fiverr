0f07c7f6a7a5a9fdc6ba7920314f0ba884ba4faa
Maxime <maxime.nicastro@gmail.com>
2021-06-29 12:36:20 +0200

    cat

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,32 @@
 import pandas as ps
 import streamlit as st
 
+
+st.sidebar.title('Hackathon Challenge')
+st.sidebar.subheader('Navigation')
+
+categorie = st.sidebar.radio("Categories", ("Accueil", "Data Cleaning","DataViz"
+                                            "Maching Learning","NLP"))
+
+
+if categorie == 'Accueil':
+    st.title('Accueil')
+
+if categorie == 'Data Cleaning':
+    st.title('Data Cleaning')
+
+if categorie == 'DataViz':
+    st.title('DataViz')
+
+if categorie == 'Maching Learning':
+    st.title('Maching Learning')
+
+if categorie == 'NLP':
+    st.title('NLP')
+
+
+
+
 # test
 
 # let's code
