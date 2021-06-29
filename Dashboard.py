e72e9f7a429e670e05f1188ae55194c4a73e3dfb
MickaelKohler <kohler.mick@gmail.com>
2021-06-29 23:13:20 +0200

    Add Ressources and databases

--- Dashboard.py
+++ Dashboard.py
@@ -57,6 +57,8 @@ if categorie == 'The Mission':
         '''
         The study is based on a database of 950 sellers on the Fiverr platform. Let’s see who are they.
         ''')
+
+    # country repartition
     countries = pd.DataFrame((df['user-stats-from 1'].value_counts(normalize=True) * 100).round(2)).iloc[:10]
     fig = go.Figure(data=[go.Scatter(
         x=list(countries.index), y=[1] * 10,
@@ -74,16 +76,132 @@ if categorie == 'The Mission':
     fig.update_layout(showlegend=False, font_family='IBM Plex Sans',
                       title='<b>Which country do Fiverrs come from ?</b>',
                       uniformtext_minsize=14, uniformtext_mode='hide',
+                      title_x=0.5, font_size=13,
                       margin=dict(l=10, r=10, b=10),
                       plot_bgcolor='rgba(0,0,0,0)')
     fig.update_yaxes(title=None, showticklabels=False)
     st.plotly_chart(fig, use_container_width=True)
 
+    # age repartition
     fig = px.histogram(df, x="user-stats-member-since 1", nbins=16, color_discrete_sequence=['#1dbf73'],
                        title='<b>What is their seniority ?</b>')
+    fig.update_layout(title_x=0.5, font_size=13)
     fig.update_xaxes(title='Years of seniority')
     fig.update_yaxes(title='Numbers of Fiverrs')
     st.plotly_chart(fig, use_container_width=True)
+
+    # level reparition
+    pivot = df.pivot_table(values='Address', index='seller-level 1', aggfunc='count')
+    pivot = pivot.sort_values('seller-level 1', ascending=False)
+    pivot = pivot.reset_index()
+
+    fig3 = px.pie(pivot, values='Address', names='seller-level 1', labels='seller-level 1',
+                  color_discrete_sequence=px.colors.sequential.Tealgrn_r, hole=.5
+                  )
+    fig3.update_layout(width=800, height=500, )
+    fig3.update_traces(textposition='inside')
+    fig3.update_traces(texttemplate="<br>%{percent:%f}", textfont_size=15)
+    fig3.update_layout(title="<b>Sellers' repartition</b>", font_size=13,
+                       title_x=0.5, title_font_family="Verdana")
+    fig3.update_layout(showlegend=False)
+    fig3.update_yaxes(color='white')
+    fig3.update_xaxes(color='white')
+    fig3.add_annotation(
+        x=5,
+        y=1000,
+        xref="x",
+        yref="y",
+        text="Level 2 Sellers",
+        showarrow=False,
+        font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+        ),
+        align="center",
+        arrowhead=2,
+        arrowsize=1,
+        arrowwidth=2,
+        ax=20,
+        ay=-30,
+        borderwidth=2,
+        borderpad=2,
+        bgcolor='#257d98',
+        opacity=0.8
+    )
+    fig3.add_annotation(
+        x=4.8,
+        y=990,
+        xref="x",
+        yref="y",
+        text="Top Sellers",
+        showarrow=False,
+        font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+        ),
+        align="center",
+        arrowhead=2,
+        arrowsize=1,
+        arrowwidth=2,
+        ax=20,
+        ay=-30,
+        borderwidth=2,
+        borderpad=2,
+        bgcolor='#4ac8a4',
+        opacity=0.8
+    )
+    fig3.add_annotation(
+        x=2,
+        y=990,
+        xref="x",
+        yref="y",
+        text="Level 1 Sellers",
+        showarrow=False,
+        font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+        ),
+        align="center",
+        arrowhead=2,
+        arrowsize=1,
+        arrowwidth=2,
+        ax=20,
+        ay=-30,
+        borderwidth=2,
+        borderpad=2,
+        bgcolor='#38b2a3',
+        opacity=0.8
+    )
+    fig3.add_annotation(
+        x=2,
+        y=1000,
+        xref="x",
+        yref="y",
+        text='No Level',
+        showarrow=False,
+        font=dict(
+            family="Courier New, monospace",
+            size=16,
+            color="#ffffff"
+        ),
+        align="center",
+        arrowhead=2,
+        arrowsize=1,
+        arrowwidth=2,
+        ax=20,
+        ay=-30,
+        borderwidth=2,
+        borderpad=2,
+        bgcolor='#2c98a1',
+        opacity=0.8
+    )
+
+    fig3.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    st.plotly_chart(fig3, use_container_width=True)
+
 
 if categorie == 'Clustering':
     st.markdown("***")
@@ -93,6 +211,63 @@ if categorie == 'Clustering':
         st.title(' ')
 
     st.subheader('Why looking for clusters of sellers ?')
+    st.markdown('''
+        text
+    
+    ''')
+
+    data = df.groupby('seller-level 1').mean()[['user-stats-member-since 1']]
+    fig = go.Figure(go.Bar(
+        x=['No Level', 'Level 1', 'Level 2', 'Top Rated'],
+        y=data['user-stats-member-since 1'],
+        marker_color=['#1dbf73']*4,
+        text=data['user-stats-member-since 1'].round(2),
+        textposition='auto'))
+    fig.update_traces(texttemplate='%{text} years', textfont_size=15)
+    fig.update_layout(showlegend=False, font_family='IBM Plex Sans',
+                      font_size=13, margin=dict(l=10, r=10, b=10, t=40),
+                      title="Sellers' seniority according to their level",
+                      title_x=.5, height=350)
+    fig.update_xaxes(title='Seller Level', dtick=1)
+    st.plotly_chart(fig, use_container_width=True)
+
+    data = df.groupby('seller-level 1').median()[['ratings-count 1', 'collect-count 1']]
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=['No Level', 'Level 1', 'Level 2', 'Top Rated'],
+                             y=data['ratings-count 1'],
+                             line=dict(color='#257d98', width=4),
+                             name='Number of rates'))
+    fig.add_trace(go.Scatter(x=['No Level', 'Level 1', 'Level 2', 'Top Rated'],
+                             y=data['collect-count 1'],
+                             line=dict(color='#38b2a3', width=4),
+                             name='Number of bookmarks'))
+    fig.update_layout(font_family='IBM Plex Sans',
+                      title="Number of ratings and bookmarks according to the sellers' level",
+                      title_x=0.5,
+                      margin=dict(l=5, r=5, b=5),
+                      height=550,
+                      hovermode="x",
+                      font_size=13,
+                      hoverdistance=100,
+                      spikedistance=1000,
+                      legend=dict(
+                          orientation="h",
+                          yanchor="bottom",
+                          y=1.02,
+                          xanchor="right",
+                          x=1)
+                      )
+    fig.update_xaxes(dtick=1, title='Seller level',
+                     linecolor="#BCCCDC",
+                     showspikes=True,
+                     spikethickness=2,
+                     spikedash="dot",
+                     spikecolor="#999999",
+                     spikemode="across"
+                     )
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.markdown('***')
 
     st.subheader("The Selected Clusters' Characteristics")
     st.markdown("""For the same number of scores approximately, but cluster 2 has less seniority,
